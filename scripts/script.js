f41f4233dd690db2056ced6541376f86e08c9117
QuinnHou <queiyun.hou@gmail.com>
2018-09-08 11:46:20 -0700

    abstracted out current location from map init

--- scripts/script.js
+++ scripts/script.js
@@ -1,62 +1,17 @@
-// This example adds a search box to a map, using the Google Place Autocomplete
-// feature. People can enter geographical searches. The search box will return a
-// pick list containing a mix of places and predicted search terms.
-
-// This example requires the Places library. Include the libraries=places
-// parameter when you first load the API. For example:
-// <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&libraries=places">
-
 function initAutocomplete() {
   // object for current position
   // var coords = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
 
   // drawing initial map
   var map = new google.maps.Map(document.getElementById('map'), {
-    center: { lat: 49.2630917, lng: -123.1142624 },
+    center: { lat:  49.2606, lng: 123.2460 },
     zoom: 13,
     mapTypeId: 'roadmap'
   });
 
-  // get current location: https://developers.google.com/maps/documentation/javascript/geolocation
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(function (position) {
-      var pos = {
-        lat: position.coords.latitude,
-        lng: position.coords.longitude
-      };
+  _currentLocation(map);
 
-      // set a marker 
-      // var currentLocationMarker = new google.maps.Marker({ position: pos, map: map });
-
-      /*
-      // set an info window
-      infoWindow = new google.maps.InfoWindow;
-
-      infoWindow.setPosition(pos);
-      infoWindow.setContent('Current Location');
-      infoWindow.open(map);
-      */
-
-      map.setCenter(pos);
-    }, function () {
-      handleLocationError(true, /* infoWindow, */ map.getCenter());
-    });
-  } else {
-    // Browser doesn't support Geolocation
-    handleLocationError(false, infoWindow, map.getCenter());
-  }
-
-  function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-    infoWindow.setPosition(pos);
-    infoWindow.setContent(browserHasGeolocation ?
-      'Error: The Geolocation service failed.' :
-      'Error: Your browser doesn\'t support geolocation.');
-    infoWindow.open(map);
-  }
-  // end of current location
-  
   // Create the search box and link it to the UI element.
-  currentLocationMarker = null;
   var input = document.getElementById('pac-input');
   var searchBox = new google.maps.places.SearchBox(input);
   map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
@@ -114,4 +69,43 @@ function initAutocomplete() {
     });
     map.fitBounds(bounds);
   });
+}
+
+
+function _currentLocation(map) {
+  console.log("currentlocation");
+  // get current location: https://developers.google.com/maps/documentation/javascript/geolocation
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function (position) {
+      var pos = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude
+      };
+      // set a marker 
+      var currentLocationMarker = new google.maps.Marker({ position: pos, map: map });
+
+      /*
+      // set an info window
+      infoWindow = new google.maps.InfoWindow;
+
+      infoWindow.setPosition(pos);
+      infoWindow.setContent('Current Location');
+      infoWindow.open(map);
+      */
+
+      map.setCenter(pos);
+    }, function () {
+      handleLocationError(true, infoWindow, map.getCenter());
+    });
+  } else {
+    // Browser doesn't support Geolocation
+    handleLocationError(false, infoWindow, map.getCenter());
+  }
+  function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+    infoWindow.setPosition(pos);
+    infoWindow.setContent(browserHasGeolocation ?
+      'Error: The Geolocation service failed.' :
+      'Error: Your browser doesn\'t support geolocation.');
+    infoWindow.open(map);
+  }
 }
--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const path    = require("path");
 try {
     // set static directories
     app.use(express.static(__dirname));
-    const port = process.env.PORT || 8000;
+    const port = process.env.PORT || 3000;
     app.get('/', function (req, res) {
         res.sendFile(path.join(__dirname+ '/index.html'));
     });
